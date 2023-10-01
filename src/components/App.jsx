af604461b501164461fdd9af94fa8ac029ce9275
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-10-01 16:45:18 +0300

    inAPP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,77 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar/searchBar";
-import ImageGallery from "./ImageGallery/imageGallery";
+import { ImageGallery } from "./ImageGallery/imageGallery";
+import ImageGalleryItem from "./ImageGalleryItem/imageGalleryItem";
 import { AppContainer } from "./AppStyle";
 import Modal from "./Modal/modal";
-
+import { Loader } from 'components/Loader/loader';
+import Button from "./Button/button";
+import { fetchPhoto } from "service/api";
 
 
 
 export default class App extends Component {
   state = {
+    searchText: null,
     search: null,
     modal: {
       isOpen: false,
       data: null,
     },
+    isLoading: false,
+    error: null,
+    page: 1,
   }
+
+  handleFormSubmit = Search => {
+    this.setState({ searchText: Search });
+  }
+
+  fetchSearchPhoto = async () => {
+    try {
+      this.setState({ isLoading: true });
+      const photo = await fetchPhoto(this.state.searchText, 1);
+      this.setState({
+        search: photo,
+        page: 1,
+      });
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+
+    }
+  }
+
+  loadMore = async () => {
+    try {
+      let nextPage = this.state.page + 1;
+      const photo = await fetchPhoto(this.state.searchText, nextPage);
+      this.setState(prevState => {
+        return {
+          search: [...prevState.search, ...photo],
+          page: nextPage,
+          isLoading: true,
+        }
+      })
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevProps.searchText;
+    const nextSearch = this.state.searchText;
+    if (prevSearch !== nextSearch) {
+      this.fetchSearchPhoto();
+    }
+  }
+
+
 
   onOpenModal = (modalData) => {
     this.setState({
@@ -35,20 +92,34 @@ export default class App extends Component {
   }
 
 
-  handleFormSubmit = Search => {
-    this.setState({ search: Search });
-  }
+
 
   render() {
-    return (
+    const showPost = Array.isArray(this.state.search) && this.state.search.length;
 
+    return (
       <AppContainer>
         <SearchBar handleFormSubmit={this.handleFormSubmit}></SearchBar>
 
-        <ImageGallery searchInfo={this.state.search}
-          onOpenModal={this.onOpenModal}>
-
+        <ImageGallery>
+          {this.state.error && <p>{this.state.error}</p>}
+          {showPost && this.state.search.map(photo => {
+            return (
+              <ImageGalleryItem onOpenModal={this.onOpenModal}
+                key={photo.id}
+                webformatURL={photo.webformatURL}
+                largeImageURL={photo.largeImageURL}
+              />
+            );
+          })
+          }
         </ImageGallery>
+        {this.state.isLoading && (
+          <Loader />)}
+        {showPost && (
+          <Button onloadMore={this.loadMore} />
+        )}
+
         {this.state.modal.isOpen && <Modal
           largeImageURL={this.state.modal.data}
           onCloseModal={this.onCloseModal} />}
--- src/components/ImageGallery/imageGallery.jsx
+++ src/components/ImageGallery/imageGallery.jsx
@@ -1,93 +1,12 @@
-import React, { Component } from 'react'
 import { ImageGalleryUl } from './imageGalleryStyle'
-import ImageGalleryItem from 'components/ImageGalleryItem/imageGalleryItem';
-import { fetchPhoto } from 'service/api';
-import Button from 'components/Button/button';
-import { Loader } from 'components/Loader/loader';
+import React from 'react'
 
-export default class ImageGallery extends Component {
-	state = {
-		search: null,
-		isLoading: false,
-		error: null,
-		page: 1,
-	}
-
-	loadMore = async () => {
-		try {
-			let nextPage = this.state.page + 1;
-			const photo = await fetchPhoto(this.props.searchInfo, nextPage);
-			this.setState(prevState => {
-				return {
-					search: [...prevState.search, ...photo],
-					page: nextPage,
-					isLoading: true,
-				}
-			})
-		} catch (error) {
-			this.setState({ error: error.message });
-		} finally {
-			this.setState({ isLoading: false });
-		}
-	}
-
-
-	fetchSearchPhoto = async () => {
-		try {
-			this.setState({ isLoading: true });
-			const photo = await fetchPhoto(this.props.searchInfo, 1);
-			this.setState({
-				search: photo,
-				page: 1,
-			});
-		} catch (error) {
-			this.setState({ error: error.message });
-		} finally {
-			this.setState({ isLoading: false });
-
-		}
-	}
-
-
-	componentDidUpdate(prevProps, prevState) {
-		const prevSearch = prevProps.searchInfo;
-		const nextSearch = this.props.searchInfo;
-		if (prevSearch !== nextSearch) {
-			this.fetchSearchPhoto();
-		}
-	}
-
-
-	render() {
-		const showPost = Array.isArray(this.state.search) && this.state.search.length;
-		return (
-			<div>
-				<ImageGalleryUl >
-
-
-					{this.state.error && <p>{this.state.error}</p>}
-
-					{/* {!this.props.searchInfo && <div>Заповніть поле пошук!</div>} */}
-					{showPost && this.state.search.map(photo => {
-						return (
-							<ImageGalleryItem onOpenModal={this.props.onOpenModal}
-								key={photo.id}
-								webformatURL={photo.webformatURL}
-								largeImageURL={photo.largeImageURL}
-							/>
-						);
-					})
-					}
-				</ImageGalleryUl>
-				{this.state.isLoading && (
-					<Loader />
-
-				)}
-				{showPost && (
-					<Button onloadMore={this.loadMore} />
-				)}
-
-			</div>
-		)
-	}
+export const ImageGallery = () => {
+	return (
+		<div>
+			<ImageGalleryUl>
+			</ImageGalleryUl>
+		</div>
+	)
 }
+
