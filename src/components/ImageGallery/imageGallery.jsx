4310e3ef4c3fcadad300c9a981b7afcaf71a326b
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-10-01 16:47:44 +0300

    Update imageGallery.jsx

--- src/components/ImageGallery/imageGallery.jsx
+++ src/components/ImageGallery/imageGallery.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 
 export const ImageGallery = () => {
 	return (
-		<div>
-			<ImageGalleryUl>
-			</ImageGalleryUl>
-		</div>
+		<ImageGalleryUl>
+		</ImageGalleryUl>
 	)
 }
 
